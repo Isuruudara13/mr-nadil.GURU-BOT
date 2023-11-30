eec0e6544a01974b1378ff3d0c49627525bdb9a5
Isuruudara13 <152253068+Isuruudara13@users.noreply.github.com>
2023-11-30 10:48:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  ['94719370832', 'me', true],
+  ['94719370832', 'mr nadil', true], 
+  ['94719370832,'mr nadil'] 
 ] //Number of owners
 
 global.mods = ['917605902011','32460248586'] 
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᵐʳ ⁿᵃᵈⁱˡ ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'mr nadil' 
+global.author = '@mr nadil' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
